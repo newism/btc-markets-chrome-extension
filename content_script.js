3a09704fb4eaba09c668a405cb1e5fd44c97adcb
Leevi Graham <leevi@newism.com.au>
2017-08-24 23:03:43 +1000

    Added total spend and profit percentage to header. Made table full width.

--- content_script.js
+++ content_script.js
@@ -4,14 +4,20 @@ window.onload = function () {
         return parseFloat(Math.round(value * 100) / 100).toFixed(2);
     };
 
-    let openOrders = document.getElementById('OpenOrders').style.width = '446px';
-    let previousTrades = document.getElementById('PreviousTrades').style.width = '526px';
+    let openOrders = document.getElementById('OpenOrders');
+        openOrders.style.float = 'none';
+        openOrders.style.width = 'auto';
+
+    let previousTrades = document.getElementById('PreviousTrades');
+        previousTrades.style.float = 'none';
+        previousTrades.style.width = 'auto';
+
+    document.getElementById('PreviousTradesContainer').style.height = 'auto';
 
     setInterval(function () {
 
         let totalDollarDifference = 0;
-
-        document.getElementById('PreviousTradesContainer').style.height = 'auto';
+        let totalBuySpend = 0;
 
         let lastPrice = document.querySelector('#BTCSummary .bid .value').innerText;
         lastPrice = Number(lastPrice.replace(/[^0-9\.]+/g, ""));
@@ -19,28 +25,31 @@ window.onload = function () {
         let previousTradesHeader = document.querySelector('#PreviousTrades > table thead');
         if (previousTradesHeader.rows[0].cells.length === 4) {
 
-            let headerProfitCell = previousTradesHeader.rows[0].insertCell(-1).innerText = 'Spend';
-            let headerCell = previousTradesHeader.rows[0].insertCell(-1).innerText = '+/-';
+            let headerSpendCell = previousTradesHeader.rows[0].insertCell(-1);
+            let headerProfitDollarCell = previousTradesHeader.rows[0].insertCell(-1);
+            let headerProfitPercentageCell = previousTradesHeader.rows[0].insertCell(-1);
             let headerCellScroll = previousTradesHeader.rows[0].insertCell(-1);
 
+            previousTradesHeader.rows[0].cells[3].style.width = 'auto';
+
+            Object.assign(headerSpendCell.style, {width:"80px", textAlign: "right"});
+            Object.assign(headerProfitDollarCell.style, {width:"80px", textAlign: "right"});
+            Object.assign(headerProfitPercentageCell.style, {width:"80px", textAlign: "right"});
             headerCellScroll.classList.add('scroll-area');
-            previousTradesHeader.rows[0].cells[0].style.width = "113px";
-            previousTradesHeader.rows[0].cells[2].style.width = "80px";
-            previousTradesHeader.rows[0].cells[3].style.width = "50px";
-            previousTradesHeader.rows[0].cells[4].style.width = "50px";
         }
 
         let previousTradesTable = document.getElementById('PreviousTradesTable');
         let rows = previousTradesTable.tBodies[0].rows;
         if(previousTradesTable.rows[0].cells.length === 5) {
             for (let i = 0; i < rows.length; ++i) {
-                let priceCell = rows[i].insertCell(4);
-                let profitCell = rows[i].insertCell(5);
 
-                rows[i].cells[0].style.width="113px";
-                rows[i].cells[2].style.width="80px";
-                rows[i].cells[3].style.width="50px";
-                rows[i].cells[4].style.width="50px";
+                let spendCell = rows[i].insertCell(4);
+                let profitDollarCell = rows[i].insertCell(5);
+                let profitPercentageCell = rows[i].insertCell(6);
+
+                Object.assign(spendCell.style, {width:"80px", textAlign: "right"});
+                Object.assign(profitDollarCell.style, {width:"80px", textAlign: "right"});
+                Object.assign(profitPercentageCell.style, {width:"80px", textAlign: "right"});
             }
         }
 
@@ -49,18 +58,20 @@ window.onload = function () {
             let buyPrice = Number(rows[i].cells[3].innerText.replace(/[^0-9\.]+/g, ""));
             let buySpend = buyVolume * buyPrice;
             let currentValue = buyVolume * lastPrice;
-
-            rows[i].cells[4].innerText = "$" + formatValue(buySpend);
-
             let dollarDifference = currentValue-buySpend;
             let percentageDifference = dollarDifference / buySpend * 100;
 
-            totalDollarDifference += dollarDifference;
+            rows[i].cells[4].innerText = "$"+formatValue(buySpend);
+            rows[i].cells[5].innerText = "$"+formatValue(dollarDifference);
+            rows[i].cells[6].innerText = "$"+formatValue(percentageDifference)+"%";
 
-            rows[i].cells[5].innerText = "$"+formatValue(dollarDifference)+ " "+formatValue(percentageDifference)+"%";
+            totalDollarDifference += dollarDifference;
+            totalBuySpend += buySpend;
         }
 
-        previousTradesHeader.rows[0].cells[5].innerText = '+/- $' + formatValue(totalDollarDifference);
+        previousTradesHeader.rows[0].cells[4].innerText = '$' + formatValue(totalBuySpend);
+        previousTradesHeader.rows[0].cells[5].innerText = '$' + formatValue(totalDollarDifference);
+        previousTradesHeader.rows[0].cells[6].innerText = formatValue(totalDollarDifference / totalBuySpend * 100)+"%";
 
     }, 1000);
-}
+};
